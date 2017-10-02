fbc44934e916684d719c616fe31a43da69c159af
Brandon Lee <brandonlee781@gmail.com>
2017-10-02 08:49:53 -0500

    Didn't work

--- src/App.ts
+++ src/App.ts
@@ -70,7 +70,7 @@ class App {
       ],
       credentials: true,
       methods: 'GET, HEAD, OPTIONS, PUT, PATCH, POST, DELETE',
-      origin: ['https://www.branlee.me/', process.env.HOME_IP],
+      // origin: ['https://www.branlee.me/', process.env.HOME_IP],
       preflightContinue: true,
     };
 
